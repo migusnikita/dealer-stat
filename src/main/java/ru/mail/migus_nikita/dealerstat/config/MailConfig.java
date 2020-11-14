4de1d44941d1c11896bd9c2bf87bb51df886956f
migus nikita <migus_nikita@mail.ru>
2020-11-14 11:46:57 +0300

    commit_4

--- src/main/java/ru/mail/migus_nikita/dealerstat/config/MailConfig.java
+++ src/main/java/ru/mail/migus_nikita/dealerstat/config/MailConfig.java
@@ -14,26 +14,26 @@ import java.util.Properties;
 @PropertySource(value = "classpath:mail.properties")
 public class MailConfig {
 
-    private Environment env;
+    private Environment environment;
 
     @Autowired
-    public MailConfig(Environment env) {
-        this.env = env;
+    public MailConfig(Environment environment) {
+        this.environment = environment;
     }
 
     @Bean
     public JavaMailSender getJavaMailSender() {
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-        mailSender.setHost(env.getRequiredProperty("Host"));
-        mailSender.setPort(Integer.parseInt(env.getRequiredProperty("Port")));
-        mailSender.setUsername(env.getRequiredProperty("email.login"));
-        mailSender.setPassword(env.getRequiredProperty("email.password"));
+        mailSender.setHost(environment.getRequiredProperty("Host"));
+        mailSender.setPort(Integer.parseInt(environment.getRequiredProperty("Port")));
+        mailSender.setUsername(environment.getRequiredProperty("email.login"));
+        mailSender.setPassword(environment.getRequiredProperty("email.password"));
 
         Properties properties = mailSender.getJavaMailProperties();
-        properties.put("mail.transport.protocol", env.getRequiredProperty("mail.transport.protocol"));
-        properties.put("mail.smtp.auth", env.getRequiredProperty("mail.smtp.auth"));
-        properties.put("mail.smtp.starttls.enable", env.getRequiredProperty("mail.smtp.starttls.enable"));
-        properties.put("mail.debug", env.getRequiredProperty("mail.debug"));
+        properties.put("mail.transport.protocol", environment.getRequiredProperty("mail.transport.protocol"));
+        properties.put("mail.smtp.auth", environment.getRequiredProperty("mail.smtp.auth"));
+        properties.put("mail.smtp.starttls.enable", environment.getRequiredProperty("mail.smtp.starttls.enable"));
+        properties.put("mail.debug", environment.getRequiredProperty("mail.debug"));
         mailSender.setJavaMailProperties(properties);
 
         return mailSender;
